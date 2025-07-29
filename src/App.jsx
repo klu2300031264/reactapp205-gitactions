b0d5c699efc9f3cebe457b96b662e28f4da53c4c
JOSHI <2300031264@kluniversity.in>
2025-07-29 22:21:57 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React Project - S205</h1>
       <p>CI/CD and Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+      
     </div>
   );
 }
